00c585845a9d02cc8066c0b72cbd25e31b3e59c8
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2020-10-11 13:39:12 -0300

    estudo 11/10/20 p4

--- functionESTUDO01.js
+++ functionESTUDO01.js
@@ -25,7 +25,6 @@ const array  = [function (nameadmin){return nameadmin}];
     console.log(array[0]('Lucas Cavalcante'));   
 
 //Uma função pode retornar/ ou conter outra função
-
 function soma (a,b){
     return (c) =>{console.log(a+b+c)}
 }
@@ -113,13 +112,28 @@ const ex03 = (c1) =>{
 
   /******************************************* */
 //criando uma função dentro do array
-
 const name = [(name1)=>{return name1}];
 console.log(name[0]('Lucas, Larissa, AnnaBelle'))
 
 /*********************************************** */
 //amarzenar dentro do atributo de um objeto
-
 const obj = {}
 obj.falar = function (){return 'opa'}
 console.log(obj.falar())
+
+/*********************************************** */
+
+//parâmetro padrão, estrategia 1 para gerar VALOR padrão
+function soma (a,b,c) {
+    a=a || 1   // colocando valor padrão 1 usando o "OU ||" para cada parâmetro
+    b=b || 1
+    c=c || 1
+    return a+b+c
+};
+console.log(soma(2), soma (4,2));   //mesmo se tiver somento 1 parâmetro retornará 1
+
+//parâmetro novo es2015
+function somar2 (a=1,b=1,c=1) {
+    return a+b+c
+};
+console.log(soma(2))
