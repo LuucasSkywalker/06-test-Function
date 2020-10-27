30532d306c29802ca0f07b467b65425622a33489
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2020-10-26 22:11:39 -0300

    Estudos Function 26/10/2020

--- functionEstudo02.js
+++ functionEstudo02.js
@@ -1,5 +1,6 @@
 // função bind e this Objeto
-//O bind é responsavel por amarrar o metodo sempre que for chamado
+/*O bind é responsavel por amarrar o metodo sempre que for chamado
+ou referenciar como exemplo abaixo "person"*/
 
 const person = {
     hello: 'good morning',
@@ -27,13 +28,44 @@ speak();  // retorna undefined, pois a const "speak" não referência o this de 
 const speak2 = person1.speak.bind(person1)
 speak2();  //retorna "good morning"
 
+
+/************************************************** */
+/*usando function normal pode variar pois o quem ta invocando
+o THIS é o temporazidor do SETINVERVAL,exemplo abaixo */
 function person3 (){
+    this.age = 0
+    setInterval(function(){   //usando função normal
+        this.age++
+        console.log(this.age)   //returna NAN pois quem invoca o this 
+                                //é o temporizador do setInterval não o 
+                                //person3
+    },1000)
+};
+new person3
+
+/*UMA OBESERVAÇÃO: se usar o "bind()" poderá amarrar o this.person3 e
+funcionará, pois o bind é usado para refenciar o person3*/
+function person4 (){
+      this.age = 0
+      setInterval(function(){
+      this.age++
+      console.log(this.age)
+      }.bind(this),1000)  //utilizando o "bind(this)" para referenciar o person4
+};
+new person4
+
+
+//o this não varia quem ta chamando usando ARROW function . Exemplo Abaixo
+function person5 (){
     
     this.age = 0
-    setInterval(()=>{
+    setInterval(()=>{   //usando ARROW function
         this.age++
-        console.log(this.age)
+        console.log(this.age)   /*usando a arrow function ele já referencia o this
+                                 o this person5 ou qualquer outro, sem precisar do
+                                 do "bind()"*/ 
        
     },1000)
 }
-new person3
+new person5
+
