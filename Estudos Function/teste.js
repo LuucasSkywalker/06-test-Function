6f355640cc502a53b6469facfd03781ff5d396a8
Lucas Cavalcante de Rezende <luucasskywalker@gmail.com>
2020-10-29 23:58:04 -0300

    Estudos Function CALLBACK formas alternativas FILTER()  29/10/2020

--- Estudos Function/teste.js
+++ Estudos Function/teste.js
@@ -7,3 +7,29 @@ for (let i = 0; i <= notas.length; i++){
 }}
 
 console.log(notasAprovadas)
+
+const notas2 = [7.7, 8.5, 4.6, 6.1, 5.8, 5.5, 8.1, 4.9]
+const notasAprovadas2 = []
+
+notas2.forEach((notas)=>{
+    if (notas >= 6){
+        notasAprovadas2.push(notas)
+    }
+    
+    
+    
+})
+console.log(notasAprovadas2)
+
+const listaApro2 = [{name:'Lucas', Note: 9.6},
+                   {name:'Larissa', Note: 7.2},
+                   {name:'Cleyri', Note: 5.6},
+                   {name:'Annabelle', Note: 5.1}]
+
+const list = []
+listaApro2.forEach((aprovados, indice)=> {
+  if (aprovados.Note >= 7){
+      list.push(aprovados)
+  }
+});
+console.log(list[0].Note)
